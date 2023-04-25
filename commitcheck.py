59dc69d15fd2743866ac71c0ea5760a0a1dcb8a3
Harish Aravindan <20925386+uptownaravi@users.noreply.github.com>
2023-04-25 14:10:38 +0530

    Create commitcheck.py

---
+++ commitcheck.py
@@ -0,0 +1,25 @@
+import re
+import argparse
+
+def commit_check(message, regex):
+
+    try:
+        #find the match using regex with the latest commit message
+        if re.search(fr"{regex}", message):
+            print("validation successful")
+            print("pattern " + regex + " is available in the commit message " + message )
+        else:
+            print("validation not successful")
+            print("pattern " + regex + " is not available in the commit message " + message )
+            output = "pattern " + regex + " is not available in the commit message " + message
+            raise ValueError(output)
+            
+    except Exception as e:
+        return('Error: ' + e)
+
+
+parser = argparse.ArgumentParser(description="Commit message validation. Send regex and message to validate")
+parser.add_argument("regex", type=str)
+parser.add_argument("message", type=str)
+args = parser.parse_args()
+commit_check(args.message, args.regex)
